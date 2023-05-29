d345ca4d7b4fc9a65a21a0e1f466a8f63dc44ec4
ericjen01 <eric.jen01@gmail.com>
2023-05-29 00:25:36 -0700

    7-a2: react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+//import { useState } from "react";
 
+const Home = () => (
+	<div>
+		<h2>TKTL notes app</h2>
+	</div>
+);
+
+const Notes = () => (
+	<div>
+		<h2>Notes</h2>
+	</div>
+);
+
+const Users = () => (
+	<div>
+		<h2>Users</h2>
+	</div>
+);
+
+const App = () => {
+	const padding = {
+		padding: 5,
+	};
+
+	return (
+		<Router>
+			<div>
+				<Link style={padding} to="/">
+					home
+				</Link>
+				<Link style={padding} to="/notes">
+					homes
+				</Link>
+				<Link style={padding} to="/users">
+					users
+				</Link>
+			</div>
+
+			<Routes>
+				<Route path="/notes" element={<Notes />} />
+				<Route path="/users" element={<Users />} />
+				<Route path="/" element={<Home />} />
+			</Routes>
+
+			<div>
+				<i>Note app, Department of Computer Science 2023</i>
+			</div>
+		</Router>
+	);
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+/*  7-a1
 const Home = () => (
 	<div>
 		<h2>TKTL notes app</h2>
@@ -66,6 +120,7 @@ const App = () => {
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+*/
 
 /*
 import ReactDOM from "react-dom/client";
